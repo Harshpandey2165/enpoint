db5ae4433df06bb7e5635b21f3afa74b67765312
Harshpandey2165 <harshpandey00009@gmail.com>
2025-04-07 23:55:23 +0530

    Update health check path to include api prefix

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { Public } from './auth/public.decorator';
+import { Public } from '../src/auth/public.decorator';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/health')
+  @Get('api/health')
   @Public()
   health() {
     return {
